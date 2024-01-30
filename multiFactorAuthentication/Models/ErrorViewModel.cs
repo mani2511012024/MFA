758a35c0ef953bfc4887731593d15f27dc75aa07
Manikandan Krish <manikandan.krish@aspiresys.com>
2024-01-30 14:59:07 +0530

    myapp

---
+++ multiFactorAuthentication/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace multiFactorAuthentication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
